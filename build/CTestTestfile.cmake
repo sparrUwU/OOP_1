0fd69b0b3ee1d5a25616fd54122106fb873bee03
sparrUwU <ikira318@gmail.com>
2025-11-12 00:50:52 +0300

    Build to gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/sparrowo/Documents/GitHub/OOP_1
-# Build directory: /mnt/c/Users/sparrowo/Documents/GitHub/OOP_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/sparrowo/Documents/GitHub/OOP_1/tests
-# Build directory: /mnt/c/Users/sparrowo/Documents/GitHub/OOP_1/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/c/Users/sparrowo/Documents/GitHub/OOP_1/build/tests/tests[1]_include.cmake")
